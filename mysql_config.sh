a61acc894ece657de0192e6827444a3448b838ec
ZhuJunJi <44352319+ZhuJunJi@users.noreply.github.com>
2019-07-08 00:29:17 +0800

    Update mysql_config.sh
    路径错误修改

--- mysql_config.sh
+++ mysql_config.sh
@@ -4,10 +4,10 @@ echo "将master目录下的配置文件复制到 docker volume /var/lib/docker/v
 cp -f ./master/mysql.cnf /var/lib/docker/volumes/${BASE_NAME}_master-conf/_data
 
 echo "将slave1目录下的配置文件复制到 docker volume /var/lib/docker/volumes/${BASE_NAME}_slave1-conf/_data"
-cp -f ./slave1/mysql.cnf /var/lib/docker/volumes/${BASE_NAME}_master-conf/_data
+cp -f ./slave1/mysql.cnf /var/lib/docker/volumes/${BASE_NAME}_slave2-conf/_data
 
 echo "将slave2目录下的配置文件复制到 docker volume /var/lib/docker/volumes/${BASE_NAME}_slave2-conf/_data"
-cp -f ./slave2/mysql.cnf /var/lib/docker/volumes/${BASE_NAME}_master-conf/_data
+cp -f ./slave2/mysql.cnf /var/lib/docker/volumes/${BASE_NAME}_slave2-conf/_data
 
 echo "重启docker-compose"
 
